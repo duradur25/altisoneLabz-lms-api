9e84c880664a1943c4d03e77397fe7074b3f5d44
duradur <dutarahmatullah25@gmail.com>
2026-07-02 08:22:05 +0700

    update description in pagination file

--- utils/pagination.py
+++ utils/pagination.py
@@ -3,8 +3,8 @@ from fastapi import Query
 class Pagination:
     def __init__(
         self,
-        page: int = Query(default=1, ge=1, description="Halaman ke berapa"),
-        limit: int = Query(default=10, ge=1, le=100, description="Jumlah data per halaman")
+        page: int = Query(default=1, ge=1, description="Which page"),
+        limit: int = Query(default=10, ge=1, le=100, description="Amount data per page")
     ):
         self.page = page
         self.limit = limit
